219ae78430b931c12941b864152c2737767d2265
Affaaf Ahmed <ahmedaffaaf@gmail.com>
2026-02-04 12:07:18 +0530

    Add line (remotely) to illustrate merge conflict

--- brian.c
+++ brian.c
@@ -4,6 +4,7 @@ int main()
 {
     printf("Im learning Git and GitHub");
     printf("I'm now adding one more line.");
+    printf("Adding one more line on GitHub!");
     printf("Adding one more line on GitHub.");
-    printf("Adding one more line on GitHub.");
+    printf("Illustrating Merge Conflict now!");
 }
