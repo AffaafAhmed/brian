1b3adfcab97ad855281247c501fa177a086871fe
AFFAAF AHMED <ahmedaffaaf@gmail.com>
2026-02-04 12:14:00 +0530

    Fix merge conflict and clean up printf statements
    Removed duplicate line and resolved merge conflict.

--- brian.c
+++ brian.c
@@ -4,12 +4,7 @@ int main()
 {
     printf("Im learning Git and GitHub");
     printf("I'm now adding one more line.");
-    printf("Adding one more line on GitHub!");
     printf("Adding one more line on GitHub.");
-<<<<<<< HEAD
-    printf("Illustrating Merge Conflict now!");
-=======
     printf("Adding one more line on GitHub.");
     printf("Illustrating Merge Conflict now.");
->>>>>>> b459885991aa960bcf12f97cf98ac1e7f1842f5d
 }
